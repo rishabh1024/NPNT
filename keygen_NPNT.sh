1fe2d09bb28ea407ed49adc4f2e33586dffb4e8c
rishabh1024 <rishabhagg2008@gmail.com>
2020-04-23 15:06:31 +0530

    Generate Self-Signed Certificate

---
+++ keygen_NPNT.sh
@@ -0,0 +1,5 @@
+openssl genrsa -out private.pem 2048
+openssl rsa -in private.pem -outform PEM -pubout -out public.pem
+openssl req -new -key private.pem -out certificate.csr
+
+openssl x509 -req -days 365 -in certificate.csr -signkey private.pem -out certificate.crt
